d5e1f48679e9467ff8529eda01f7a31792ada8f6
xieww <2801416900@qq.com>
2020-07-20 22:37:16 +0800

    feat(init): init

---
+++ main.js
@@ -0,0 +1,24 @@
+import { ToyReact, Component } from "./toyReact.js";
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        <span>hello</span>
+        <span>world!</span>
+        <div>
+          {true}
+          {this.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+let a = (
+  <MyComponent name="a" id="ida">
+    <div>123</div>
+  </MyComponent>
+);
+
+ToyReact.render(a, document.body);
